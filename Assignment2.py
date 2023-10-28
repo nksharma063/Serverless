27c7766f415454f4fb6b285830f1998afb045b9a
Neeraj <47555491+nksharma063@users.noreply.github.com>
2023-10-28 08:46:49 +0530

    Update Assignment2.py
    Added some changes in the code

--- Assignment2.py
+++ Assignment2.py
@@ -2,19 +2,19 @@ import boto3
 from datetime import datetime, timedelta
 
 def lambda_handler(event, context):
-    s3 = boto3.client('s3')
-    bucket_name = 'your-bucket-name'  # Replace with your bucket name
+    neeraj_s3 = boto3.client('s3')
+    bucket_name = 'neeraj-s3-test'  # Replace with your bucket name
 
     # Calculate the date 30 days ago
     thirty_days_ago = (datetime.now() - timedelta(days=30)).isoformat()
 
     # List objects in the specified bucket
-    objects = s3.list_objects_v2(Bucket=bucket_name)
+    objects = neeraj_s3.list_objects_v2(Bucket=neeraj-s3-test)
 
     # Delete objects older than 30 days
     for obj in objects.get('Contents', []):
         if obj['LastModified'].isoformat() < thirty_days_ago:
-            s3.delete_object(Bucket=bucket_name, Key=obj['Key'])
+            neeraj_s3.delete_object(Bucket=neeraj-s3-test, Key=obj['Key'])
 
             # Print the names of deleted objects for logging purposes
             print("Deleted:", obj['Key'])
