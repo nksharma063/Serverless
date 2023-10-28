de999461cede8bf41d8b73352ee038bb135d799a
Neeraj <47555491+nksharma063@users.noreply.github.com>
2023-10-28 08:36:16 +0530

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -1,16 +1,17 @@
 import boto3
 
 def lambda_handler(event, context):
-    # Replace 'your_rds_instance_name' with the name of your RDS instance
-    rds_instance_name = 'your_rds_instance_name'
+    # Replace 'neeraj_rds' with the name of your RDS instance
+    rds_instance_name = 'neeraj_rds'
     
     # Initialize a boto3 RDS client
     rds_client = boto3.client('rds')
 
     try:
         # Take a snapshot of the specified RDS instance
+        snapshot_identifier = f'{rds_instance_name}-snapshot-{event["time"]}'  # Customize the snapshot identifier
         response = rds_client.create_db_snapshot(
-            DBSnapshotIdentifier=f'{rds_instance_name}-snapshot-{event["time"]}',  # You can customize the snapshot identifier
+            DBSnapshotIdentifier=snapshot_identifier,
             DBInstanceIdentifier=rds_instance_name
         )
 
@@ -21,4 +22,3 @@ def lambda_handler(event, context):
     except Exception as e:
         print(f'Error creating snapshot: {str(e)}')
         raise e
-
